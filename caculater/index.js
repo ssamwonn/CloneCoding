0660308927a5b489075478c678ae2076b294c2c9
ssamwonn <42404271+ssamwonn@users.noreply.github.com>
2020-07-01 23:36:50 +0900

    Add files via upload

--- caculater/index.js
+++ caculater/index.js
@@ -1,32 +1,80 @@
 const numberInput = document.querySelector('#input'),
-    clear_Btn = document.querySelector('#clear'),
     plus_Btn = document.querySelector('#plus'),
     minus_Btn = document.querySelector('#minus'),
     divide_Btn = document.querySelector('#divide'),
     multiple_Btn = document.querySelector('#multiple'),
+    clear_Btn = document.querySelector('#clear'),
     calculate_Btn = document.querySelector('#calculate'),
     resultInput = document.querySelector('#result');
-
 
 let temp;
 let operator;
 
 plus_Btn.addEventListener('click', () => {
-    if (numberInput.value) {
-        temp = Number(numberInput.value)
+    if (temp) {
         operator = '+'
         numberInput.value = null
+    } else {
+        if (numberInput.value) {
+            temp = Number(numberInput.value)
+            numberInput.value = null
+            resultInput.value = null
+        }
+    }
+})
+minus_Btn.addEventListener('click', () => {
+    if (temp) {
+        operator = '-'
+        numberInput.value = null
+    } else {
+        if (numberInput.value) {
+            temp = Number(numberInput.value)
+            numberInput.value = null
+            resultInput.value = null
+        }
+    }
+})
+
+multiple_Btn.addEventListener('click', () => {
+    if (temp) {
+        operator = '*'
+        numberInput.value = null
+    } else {
+        if (numberInput.value) {
+            temp = Number(numberInput.value)
+            numberInput.value = null
+            resultInput.value = null
+        }
+    }
+})
+
+divide_Btn.addEventListener('click', () => {
+    if (temp) {
+        operator = '/'
+        numberInput.value = null
+    } else {
+        if (numberInput.value) {
+            temp = Number(numberInput.value)
+            numberInput.value = null
+            resultInput.value = null
+        }
     }
 })
 
 
+
+
 clear_Btn.addEventListener('click', () => {
-    numberInput.value = null;
-    temp = null;
-    operator = null;
+    resultInput.value = null
+    numberInput.value = null
+    temp = null
+    operator = null
+
 })
 
+
 calculate_Btn.addEventListener('click', () => {    //계산하는 식 
+    console.log(temp, operator, numberInput.value)
     if (operator) {
         if (numberInput.value) {
             if (operator === '+') {
@@ -37,9 +85,8 @@ calculate_Btn.addEventListener('click', () => {    //계산하는 식
                 resultInput.value = temp * Number(numberInput.value)
             } else if (operator === '/') {
                 resultInput.value = temp / Number(numberInput.value)
-            } else {
-
             }
+            temp = Number(resultInput.value);
         }
     } else {
         if (numberInput.value) {
